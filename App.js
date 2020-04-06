e43b981583b69dfedec1e40afb27fd806927b849
shashikumar2 <shashikumarbb@gmail.com>
2020-04-06 22:52:09 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react' 
+import Login from './Login'
+import Dashboard from './Dashboard'
+
+
+
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+
+function App(props) {
+    return (
+        <BrowserRouter>
+            <div>
+                
+                
+                
+               
+            
+                
+            <Route path="/" component={Login} exact={true} />
+               <Redirect to="/dashboard" component={Dashboard}  />
+               
+
+                
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App
